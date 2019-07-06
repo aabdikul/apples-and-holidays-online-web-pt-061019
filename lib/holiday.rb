755c62732c1f93989251eb146f316fa301b3ea37
Aida Abdikulova <abdikulova@gmail.com>
2019-07-06 01:25:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |nest_holiday,supply|
-      puts "  #{nest_holiday.to_s.split("_").collect {|word| word.capitalize}.join(" ")}: #{supply.join(", ")}"
+      puts "  #{nest_holiday.to_s.split("_").collect {|waord| word.capitalize}.join(" ")}: #{supply.join(", ")}"
 end
 end
 end
